e7dc9664bafdc8d5956485cda3f583fe07ed0abc
Đinh Quang Vũ <vudqgg@gmail.com>
2017-07-26 15:16:50 +0700

    Initial Commit

---
+++ Foodsy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodsy
+//
+//  Created by VuDev on 7/26/17.
+//  Copyright © 2017 VuDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
